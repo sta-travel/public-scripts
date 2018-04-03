11477f4e8eeaf18d09b8441322e77f2c2595bb73
Matt Donovan <matt.donovan@statravel.com>
2018-04-03 14:12:08 +0100

    Added yum update and salt-call to the build script

--- LinuxBuildScripts/buildsaltminion.sh
+++ LinuxBuildScripts/buildsaltminion.sh
@@ -6,19 +6,35 @@
 
 # will need a commandline variable for environment name and profile names
 
+# Set a bunch of variables
 TemplateName=$1
 ProfileName=$2
 EnvironmentName=$3
 HostName=`hostname`
 
-#
+# Download the correct template from github and name it minion for ease of access
 wget https://raw.githubusercontent.com/sta-travel/public-scripts/master/SaltConfigurations/$TemplateName-template  -O minion
 
+
+# Update parameters in the salt file to reflect the environment
 sed -i -e "s/<ProfileName>/$ProfileName/g" minion
 sed -i -e "s/<EnvironmentName>/$EnvironmentName/g" minion
 
+
+# Copy the minion file to the correct location
 sudo cp minion /etc/salt/minion 
+
+# set the salt ID to the servers hostname
 $HostName > /etc/salt/minion_id 
+
+# add the servers shortname to hosts 
 echo "127.0.0.1 $HostName" >> /etc/hosts
 
+# Update the server
+yum update -y
+
+# Restart the salt service
 service salt-minion restart
+
+# Call the salt state
+salt-call state.highstate
