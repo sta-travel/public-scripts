48b43b58559d5001532ca3fca2ee9069ef306824
ianseaman <ian.seaman@statravel.com>
2018-05-01 16:53:18 +0100

    centos 6 versions added

--- LinuxBuildScripts/snmpdconf.sh
+++ LinuxBuildScripts/snmpdconf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Install and Configure snmpd and firewall on Centos 7 to enable monitoring by Solarwinds
+# Install and Configure snmpd and firewall on Centos 7 to enable monitoring by Solarwinds or any snmp based monitor
+# snmpd.conf will need network settings edited to suit the environemnt
 yum install -y net-snmp-utils net-snmp
 cd /etc/snmp
 mv snmpd.conf snmpd.conf.orig
@@ -8,3 +9,4 @@ cd /etc/firewalld/services
 wget https://github.com/sta-travel/public-scripts/blob/master/LinuxBuildScripts/snmp.xml
 firewall-cmd --zone=public --add-service snmp --permanent
 firewall-cmd --reload
+service snmpd restart
